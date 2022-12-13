5c70a9000e2ddbc5c6d8500752d4927fbd591407
sahilbalgotra100 <sharmagopi435@gmail.com>
2022-12-13 20:52:07 +0530

    added twak too

--- resources/views/website/layouts/script.blade.php
+++ resources/views/website/layouts/script.blade.php
@@ -5,6 +5,23 @@
 <script src="{{ asset('website/js/swiper.min.js') }}"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="{{ asset('website/js/script.js?v=') }}{{ time() }}"></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/608a548b62662a09efc32fc1/1f4e5l3pq';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 @stack('js')
 
 <!-- END of Scripts -->
