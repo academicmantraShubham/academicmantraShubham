6724b5f43e07e04ed42ffd40941e02820ae0941f
Sahil Balgotra <sharmagopi435@gmail.com git config --global user.name sahil>
2023-05-09 21:49:09 +0530

    add data

--- resources/views/website/pages/detail.blade.php
+++ resources/views/website/pages/detail.blade.php
@@ -1,23 +1,24 @@
 @extends('website.layouts.app')
 @section('headerData')
     <title>{{ $bolgDetails->meta_title }} - Best Essay Writing Services</title>
-    <meta name="description"
-        content="{{ $bolgDetails->meta_keyword }}">
-    <meta name="keywords"
-        content="{{ $bolgDetails->meta_description }}">
+    <meta name="description" content="{{ $bolgDetails->meta_keyword }}">
+    <meta name="keywords" content="{{ $bolgDetails->meta_description }}">
 @endsection
 @section('content')
     <main id="" style="padding-top: 5.5rem;">
 
         <!-- ======= Breadcrumbs ======= -->
-        <section id="breadcrumbs" class="breadcrumbs" >
+        <section id="breadcrumbs" class="breadcrumbs">
             <div class="container">
                 <ol class="breadcrumb">
                     <li><a href="/">Home</a></li>
                     <li><a href="/">Blogs</a></li>
-                    <li>{{ strip_tags($bolgDetails->title ) }}</li>
+                    <li>{{ strip_tags($bolgDetails->title) }}</li>
                 </ol>
                 <h1 class="mb-4 text-center">{!! $bolgDetails->title !!}</h1>
+                <img class="img-fluid"
+                    src="{{ $blog->featured_image ? $blog->featured_image->getUrl() : '/images/homepage/1646737989.svg' }}"
+                    alt="{{ $blog->title }}">
             </div>
         </section>
         <!-- End Breadcrumbs -->
