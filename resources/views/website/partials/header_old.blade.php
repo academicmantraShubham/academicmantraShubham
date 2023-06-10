3f7baeeda993ab98a1b12e4804d4440d54d02c1d
Sahil Balgotra <sharmagopi435@gmail.com git config --global user.name sahil>
2023-06-10 22:05:09 +0530

    Menu added

--- resources/views/website/partials/header_old.blade.php
+++ resources/views/website/partials/header_old.blade.php
@@ -1,9 +1,10 @@
+
 <!-- ======= Header ======= -->
 <!-- Navigation --> 
 <nav id="navbar" class="navbar navbar-expand-lg fixed-top navbar-dark" aria-label="Main navigation">
    <div class="container">
       <!-- Image Logo -->
-       <a class="navbar-brand logo-image" href="/"><img src="{{$logo->image}}" alt="{{$logo->alt}}"></a> 
+      <a class="navbar-brand logo-image" href="/"><img src="{{$logo->image}}" alt="{{$logo->alt}}"></a> 
       <!-- Text Logo - Use this if you don't have a graphic logo -->
       <!--<a class="navbar-brand logo-text" href="/">BEWS</a>-->
       <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
@@ -12,18 +13,29 @@
       <div class="navbar-collapse offcanvas-collapse" id="navbarNavDropdown">
          <ul class="navbar-nav ms-auto navbar-nav-scroll">
             @foreach ($menus as $menu) 
-               @if (count($menu->subMenus) > 0)
-                  <li class="nav-item dropdown">
-                     <a class="nav-link dropdown-toggle" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false" href="#">{{ $menu->title }} <i class="bi bi-chevron-down"></i></a>
+               @if ($menu->subMenus->count() > 0)
+                  <li class="nav-item dropdown" >
+                     <a class="nav-link dropdown-toggle" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false" href="javascript:void(0)">{{ $menu->title }} <i class="bi bi-chevron-down"></i></a>
                      <ul class="dropdown-menu" aria-labelledby="dropdown01">
                         @foreach ($menu->subMenus as $subMenu)
-                           @if (count($subMenu->subMenus) > 0)
+                           @if ($subMenu->subMenus->count() > 0)
                               <li class="dropdown-submenu">
                                  <a class="dropdown-item dropdown-toggle" href="#">{{ $subMenu->title }}</a>
-                                 <ul class="dropdown-menu">
+                                 <ul class="dropdown-menu new_id" id="new_id_{{ $subMenu->id }}">
                                     @foreach ($subMenu->subMenus as $subChlid)
-                                       <li><a class="dropdown-item" href="{{ url($subChlid->slug) }}">{{ $subChlid->title }}</a></li>
-                                    @endforeach
+                                       @if ($subChlid->subMenus->count() > 0)
+                                       <li class="dropdown-submenu">
+                                          <a class="dropdown-item dropdown-toggle" href="#">{{ $subChlid->title }}</a>
+                                          <ul class="dropdown-menu new_id child-sub-menu" id="new_id_{{ $subMenu->id }}">
+                                             @foreach ( $subChlid->subMenus as $subChlidSub)
+                                                <li><a class="dropdown-item" href="{{ $subChlidSub->content ? url($subChlidSub->slug) : 'javascript:void(0)' }}">{{ $subChlidSub->title }}</a></li>
+                                             @endforeach
+                                          </ul>
+                                       </li>
+                                       @else
+                                          <li><a class="dropdown-item" href="{{ $subChlid->content ? url($subChlid->slug) : 'javascript:void(0)' }}">{{ $subChlid->title }}</a></li>
+                                       @endif
+                                    @endforeach 
                                  </ul>
                               </li>
                            @else
@@ -34,22 +46,22 @@
                   </li>
                @else
                   <li class="nav-item">
-                     <a class="nav-link @if($loop->first) active @endif" aria-current="page"  href="{{ url($menu->slug) }}">{{ $menu->title }}</a>
+                     <a class="nav-link @if($loop->first) active @endif" aria-current="page"  href="{{ $menu->content ? url($menu->slug) : 'javascript:void(0)' }}">{{ $menu->title }}</a>
                   </li>
                @endif 
             @endforeach
          </ul>
          <span class="nav-item social-icons">
             <span class="fa-stack">
-               <a href="#your-link">
+               <a href="https://www.facebook.com/bestessaywritingservicesau">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-facebook-f fa-stack-1x"></i> 
                </a>
             </span>
             <span class="fa-stack">
-               <a href="#your-link">
+               <a href="https://www.instagram.com/best_essaywritingservices/">
                   <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-twitter fa-stack-1x"></i>
+                  <i class="fab fa-instagram fa-stack-1x"></i>
                </a>
             </span>
          </span>
@@ -60,3 +72,4 @@
 </nav>
 <!-- end of navbar -->
 <!-- end of navigation -->
+
