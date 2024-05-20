9307c5513dde1c807a5b14a60d6ec024d1448a60
Sahil Balgotra <sharmagopi435@gmail.com git config --global user.name sahil>
2024-05-21 00:51:22 +0530

    changes

--- resources/views/website/partials/header.blade.php
+++ resources/views/website/partials/header.blade.php
@@ -16,12 +16,12 @@
                @if ($menu->subMenus->count() > 0)
                   <li class="nav-item dropdown" >
                      <a class="nav-link dropdown-toggle" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false" href="javascript:void(0)">{{ $menu->title }} <i class="bi bi-chevron-down"></i></a>
-                     <ul class="dropdown-menu" aria-labelledby="dropdown01">
+                     <ul class="dropdown-menu {{ $key == 2 ? 'coulum-2' : ''}} " aria-labelledby="dropdown01">
                         @foreach ($menu->subMenus as $subMenu)
                            @if ($subMenu->subMenus->count() > 0)
                               <li class="dropdown-submenu">
                                  <a class="dropdown-item dropdown-toggle second" href="#">{{ $subMenu->title }}</a>
-                                 <ul class="dropdown-menu new_id {{ $key == 2 ? 'coulum-2' : ''}} " id="new_id_{{ $subMenu->id }}">
+                                 <ul class="dropdown-menu new_id" id="new_id_{{ $subMenu->id }}">
                                     @foreach ($subMenu->subMenus as $subChlid)
                                        @if ($subChlid->subMenus->count() > 0)
                                        <li class="dropdown-submenu">
