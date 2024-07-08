c16c42ed827e9d2c1be454b20d2cea90664a3c28
Sahil Balgotra <sharmagopi435@gmail.com git config --global user.name sahil>
2024-07-08 23:50:36 +0530

    pop up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Menu;
+use App\Models\Homepage;
+use App\Models\Calculator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer(['errors::404','errors::500'], function($view)
+        {
+            $data['calculators'] = Calculator::whereNotNull('place')->get();
+            $data['team'] = Homepage::wherePage('team')->with('subHomepages')->get();
+            $data['menus'] =  Cache::remember('menus', 60, function () {
+                return Menu::with('subMenus.subMenus.subMenus')->orderBy('position', 'ASC')->whereParentId(0)->whereIsPage(0)->whereStatus(1)->get(['id', 'title', 'slug', 'content']);
+            });
+            $data['subscribeus'] =  Cache::remember('subscribeus', 60, function () {
+                return Homepage::wherePage('subscribe us')->first();
+            });
+            $data['bestoffers'] = Homepage::wherePage('best offers')->with('subHomepages')->get();
+            $data['askedquestions'] = Homepage::wherePage('asked questions')->with('subHomepages')->get();
+            $data['logo'] = Homepage::select(['image', 'alt'])->wherePage('logo')->first();
+            $data['footerdata'] = Homepage::wherePage('footer data')->first();
+            $data['pages'] = Cache::remember('pages', 60, function () {
+                return Menu::orderBy('position', 'ASC')->whereParentId(0)->whereIsPage(1)->get(['id', 'title', 'slug', 'content']);
+            });
+            $view->with($data);
+        });
     }
 }
