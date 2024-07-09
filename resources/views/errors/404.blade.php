4a61ec7f4b20347af502fb3075214c77bfcc35e0
Sahil Balgotra <sharmagopi435@gmail.com git config --global user.name sahil>
2024-07-09 12:57:24 +0530

    pop up

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -17,16 +17,16 @@
             <div class="row">
                 <div class="col-sm-12 text-center">
                     <div class="news-img-box">
-                        <img src="{{ asset('images/404.svg') }}" alt="" class="img-fluid">
+                        <img src="{{ asset('images/404.svg') }}" alt="404 not found" class="img-fluid" style="max-width:500px;">
                     </div>
                 </div>
-                <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2 text-center mt-4">
+                {{-- <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2 text-center mt-4">
                     <div class="post-content color-text-a">
                         <p class="post-intro">
                             <span class="color-b">404</span> Page Not Found
                         </p>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </section><!-- End Post Single-->
